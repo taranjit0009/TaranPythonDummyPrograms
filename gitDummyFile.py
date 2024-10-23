686129f52707444e3b4427039f507575031bfd02
taranjit0009 <taranjitsingh095@gmail.com>
2024-10-23 23:10:14 +0530

    New testerBranch has been added by tech lead

--- gitDummyFile.py
+++ gitDummyFile.py
@@ -1,9 +1,15 @@
 print("This is the files which is created by the other teammate.")
 
+class ABC:
+    def taraDef(self):
+        print("Now Taran has done his work on this file.")
 
-def taraDef():
-    print("Now Taran has done his work on this file.")
+
+    def taraDef1(self):
+        print("New changes for testerBranch1.")
 
 
 
-taraDef()
+obj = ABC()
+obj.taraDef()
+obj.taraDef1()
---
+++ gitDummyFile1.py
@@ -0,0 +1,14 @@
+print("This is the files which is created by the other teammate.")
+
+
+def taraDef():
+    print("Now Taran has done his work on this file.")
+
+
+def taraDef1():
+    print("New changes for testerBranch1.")
+
+
+
+taraDef()
+taraDef1()

5267cdb0ea0665d18ed85092b433999c5ba3c7c5
taranjit0009 <taranjitsingh095@gmail.com>
2024-10-23 19:36:25 +0530

    4th Taran has commit

--- OtherTeamMate.py
+++
@@ -1,1 +0,0 @@
-print("This is the files which is created by the other teammate.")
---
+++ gitDummyFile.py
@@ -0,0 +1,9 @@
+print("This is the files which is created by the other teammate.")
+
+
+def taraDef():
+    print("Now Taran has done his work on this file.")
+
+
+
+taraDef()
